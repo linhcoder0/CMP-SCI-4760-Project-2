383035a86c13c3f075ee5cdcc74e9dce06b396c6
linhcoder0 <114890267+linhcoder0@users.noreply.github.com>
2026-02-24 04:04:59 -0600

    Add code to parse options and receive the command parameters.

--- oss.c
+++ oss.c
@@ -2,6 +2,7 @@
 //then fork off one child and have that child access the shared memory and
 //ensure it works.
 
+//Code to parse options and receive the command parameters
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,12 +10,52 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/wait.h>
+#include <getopt.h> 
 
 const size_t BUFF_SZ = sizeof(int) * 2; 
 // had to change int to size_t to avoid compiler warning 
 //about comparison between signed and unsigned integers 
 
-int main() {
+int main(int argc, char *argv[]) {
+    // initialize variables for options
+    int n = 1;  
+    int s = 1;  
+    float t = 1.0f; 
+    float i = 0.0f;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "hn:s:t:i:")) != -1) { //parse CL options using getopt
+        switch (opt) {
+            case 'h':
+                printf("oss [-h help] [-n processes] [-s simulations] [-t time] [-i interval]\n");
+                return 0;
+            case 'n':
+                n = atoi(optarg);
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 't':
+                //strtof converts string to float, 
+                //and we use NULL for the second argument since we don't need it
+                //better than atof since it provides error checking
+                t = strtof(optarg, NULL);
+                break;
+            case 'i':
+                i = strtof(optarg, NULL);
+                break;
+            default:
+                printf("Usage: %s [-h] [-n proc] [-s simul] [-t iter]\n", argv[0]);
+                return 1;
+        }
+    } 
+    //avoid invalid input
+    if (n <= 0) n = 1;
+    if (s <= 0) s = 1;
+    if (t <= 0) t = 1.0f;
+    if (i <= 0) i = 1.0f;
+    if (s > n) s = n; //we cannot have more simul processes than total processes  
+    
     key_t shm_key = ftok("oss.c", 0);
     //int shm_key changed to key_t 
     //to match the return type of ftok, 
@@ -42,7 +83,9 @@ int main() {
     int *nano = &(clock[1]);
     *sec = *nano = 0;
 
-    printf("OSS PID:%d PPID:%d\n", getpid(), getppid());
+    printf("OSS starting, PID:%d PPID:%d\n", getpid(), getppid());
+    //At the start of oss, print out the parameters passed to it.
+    printf("Called with:\n-n %d\n-s %d\n-t %.3f\n-i %.3f\n", n, s, t, i);
     printf("OSS initialized clock: sec=%d nano=%d\n", clock[0], clock[1]);
 
     pid_t child_pid = fork();
